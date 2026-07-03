2748929ad76b8b89f8219a865bcfac7781619f49
Max Garfinkel <maxg@mo.co.uk>
2026-07-03 16:42:37 +0100

    MG Update base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/aristotle/',
   plugins: [tailwindcss(), react()],
   test: {
     environment: 'jsdom',
